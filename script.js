cd12215cc43ba5bc15b264d7d4870d9bec6515c2
Terry <kwok876@gmail.com>
2021-12-11 22:00:11 +0800

    update scroll bugged ver

--- script.js
+++ script.js
@@ -11,6 +11,13 @@ const contactBtn = document.querySelector(".contactBtn");
 const navBar = document.querySelector(".nav-bar");
 const menuBtn = document.querySelector(".menuBtn");
 
+const wrapper = document.querySelector(".wrapper");
+const scrollContainer = document.querySelector(".scroll-container");
+const scrollLeftBtn = scrollContainer.querySelector(".left");
+const scrollRightBtn = scrollContainer.querySelector(".right");
+
+let scroll = null;
+let scrollOffset = 0;
 
 const getElementPos = (elem) => {
     return elem.offsetTop - home.offsetTop;    
@@ -49,4 +56,65 @@ menuBtn.addEventListener('click', (e)=>{
     } else {
         navBar.setAttribute("data-expanded", true);
     }
+});
+
+/*
+wrapper.addEventListener('scroll', ()=>{
+    //console.log(wrapper.scrollWidth - wrapper.clientWidth);
+    console.log(wrapper.scrollLeft);    
+});
+*/
+
+const scrollContX = (isLeft)=>{
+    clearInterval(scroll);
+    scrollOffset = wrapper.scrollLeft;
+    scroll = setInterval(()=>{
+        console.log("test1");
+        wrapper.scrollTo({             
+            left: isLeft ? (scrollOffset -= 25) : (scrollOffset += 25),
+            behavior: 'smooth'
+        });
+        if(scrollOffset < 1 || scrollOffset >= wrapper.scrollWidth - wrapper.clientWidth) {
+            console.log("test2" + wrapper.scrollLeft);
+            clearInterval(scroll);
+        }
+    }, 100); 
+};
+
+const scrollX = (isLeft)=>{
+    let wrapperWidth = wrapper.offsetWidth;
+    console.log("test");
+
+};
+
+scrollLeftBtn.addEventListener('click', ()=>{
+    console.log("test0");
+    scrollLeftBtn.setAttribute("data-click", true);
+    scrollX(true);      
+    scrollLeftBtn.setAttribute("data-click", false);  
+});
+
+scrollLeftBtn.addEventListener('mouseover', ()=>{
+    if(scrollLeftBtn.getAttribute("data-click") === "false") {
+        console.log("test0");   
+        scrollContX(true); 
+    }
+           
+});
+
+scrollLeftBtn.addEventListener('mouseout', ()=>{
+    console.log("test2");   
+    clearInterval(scroll);       
+});
+
+
+
+scrollRightBtn.addEventListener('mouseover', ()=>{    
+    console.log("test0");   
+    scrollContX(false);  
+});
+
+scrollRightBtn.addEventListener('mouseout', ()=>{
+    console.log("test2");   
+    clearInterval(scroll);       
 });
