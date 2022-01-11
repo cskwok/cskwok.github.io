caef28f9a01e2dbb0b6a482ea6b306148c696fbe
Terry <kwok876@gmail.com>
2022-01-11 19:28:32 +0800

    cancel resize animation

--- script.js
+++ script.js
@@ -118,3 +118,13 @@ scrollRightBtn.addEventListener('click', (e)=>{
         scrollRight();
     }     
 });
+
+let resizeTimer;
+
+window.addEventListener("resize", () => {
+  document.body.classList.add("cancel-resize-transition");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("cancel-resize-transition");
+  }, 400);
+});
